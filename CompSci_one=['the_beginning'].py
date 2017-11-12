34b31b2ec2c4679066a40b52fb5c4034fd2bb847
Emily Lockwood <31575423+lockwe@users.noreply.github.com>
2017-11-12 17:14:30 -0500

    Add files via upload

---
+++ CompSci_one=['the_beginning'].py
@@ -0,0 +1,28 @@
+CompSci_one = ['the_beginning']
+
+def initely_there_was(a_time):
+    
+    while I_was_still in CompSci_one:
+        that_I_thought_code = easy
+        
+    for when_I_was in CompSci_one:
+        everything = 'breezy'
+        
+    if I_was_still in that_class:
+        def initions_would_be_a(breeze):
+            for when_I_was in that_class:
+                my=str(engths_were_some_of_these)
+    id(rather_stop)
+    while this == True:
+        Before("languedges are hard")
+    if I_dont_stop: 
+        I_know_its == True
+    C ++ 'will_leave_me_scared'
+
+            
+            
+            
+            
+        
+        
+    
